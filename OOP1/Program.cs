59654c2ab36218f33e70f711e9a87373669abeb1
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-04-16 17:40:34 +0300

    Okey

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,20 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product product1 = new Product();
+            product1.ID = 1;
+            product1.CategoryID = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 5;
+
+            Product product2 = new Product { ID = 2, CategoryID = 5, ProductName = "Kalem", UnitsInStock = 3, UnitPrice = 23 };
+
+            //PascalCase   //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+
         }
     }
 }
