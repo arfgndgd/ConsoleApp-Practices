71410c6b9ce0f5b67758b6f90470e1222c7fec48
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-04-17 15:15:50 +0300

    Constructor

---
+++ Constructors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+
+            Customer customer2 = new Customer() { ID=1, FirstName= "Ahmet", LastName= "Gündoğdu"};
+            Console.WriteLine(customer2.FirstName);
+
+            //hata almamak için constructora parametre vermemiz lazım çünkü constructorlar method mantığında çalışır 
+
+            Customer customer1 = new Customer(2,"Arif","Gündoğdu");
+            Console.WriteLine(customer1.FirstName);
+
+            //iki farklı durumda yani hem parametreli hem parametresiz çalıştırmak istediğimizde iki tane constructor açar isek sorun çözülür
+        }
+    }
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        //default constructor ; aslında burada proje çalıştığında default constructor vardır. Instance alındığında zaten new Customer() dediği için aynı görevi yapar.
+
+
+        public Customer(int id, string firstName, string lastName)
+        {
+            ID = id;
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
