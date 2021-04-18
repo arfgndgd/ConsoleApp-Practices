5daa29ae31237b6a0ed3191b7d55c844ba457609
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-04-19 01:48:41 +0300

    GameProject

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    //MicroService
+    class GamerManager : IGamerService
+    {
+        //bir manager sınıfının içinde başka bir manager sınıfını kullanacak isek asla onu new etmememiz gerekir
+
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer) == true)
+            {
+                Console.WriteLine("Kayıt Oldu");
+            }
+            else
+            {
+                Console.WriteLine("Kayıt Başarısız, doğrulama hatalı.");
+            }
+            
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi");
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        //doğru ya da yanlış sonucu olacağı için void yerine "bool" yapmak daha doğru 
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1996 && gamer.FirstName == "Arif" && gamer.LastName == "Gündoğdu" &&  gamer.IdentityNumber ==12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
